4f016aa1d97813c6c3b298536205dcd97851bf8c
EmmanuelZiviello <emmanuelziviello@gmail.com>
2024-05-13 19:32:18 +0200

    ""

--- src/main/java/com/example/backendingsw/Controller/Asta_allingleseController.java
+++ src/main/java/com/example/backendingsw/Controller/Asta_allingleseController.java
@@ -210,8 +210,8 @@ public class Asta_allingleseController {
         }
     }
 
-    @PostMapping("/insertAstaInglese/{asta_inglese}/{lista_categorie}")
-    public Long insertAstaInglese(@RequestBody Asta_allinglese_DTO asta_inglese_dto, @PathVariable(value = "lista_categorie", required = false) ArrayList<String> lista_categorie,@PathVariable(value = "asta_inglese") Asta_allinglese_DTO asta_inglese){
+    @PostMapping("/insertAstaInglese/{lista_categorie}")
+    public Long insertAstaInglese(@RequestBody Asta_allinglese_DTO asta_inglese_dto, @PathVariable(value = "lista_categorie", required = false) ArrayList<String> lista_categorie){
         try{
             Long lastInsertedId = asta_allingleseRepository.getLastInsertedId();
             Long id = (lastInsertedId != null) ? lastInsertedId + 1 : 1L; // Gestisce il caso in cui lastInsertedId è null
@@ -241,7 +241,7 @@ public class Asta_allingleseController {
     @GetMapping("/getEmailVincente/{indirizzo_email}/{idAstaInglese}")
 
     public Boolean getEmailVincente(@PathVariable String indirizzo_email,@PathVariable Long idAstaInglese){
-        System.out.println("entrato in getemail vincente con id e email: "+ idAstaInglese + ", " + indirizzo_email);
+
         try{
             String email_vincente = null;
             email_vincente = i_asta_allinglese_service.getEmailVincente(idAstaInglese);
