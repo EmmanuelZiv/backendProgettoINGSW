4b6146d40a95a3a46a5930e0e3fe5b5783d8a32b
EmmanuelZiviello <emmanuelziviello@gmail.com>
2024-05-13 22:29:46 +0200

    Revert "Ora nella home vengono recuperato al massimo 15 aste per tipo (per categoria, per scadenza recente ecc..)"
    This reverts commit b7749e8671e203437c11403166c4289c17c8ee2d.

--- src/main/java/com/example/backendingsw/Service/Implementations/Impl_Asta_inversa_Service.java
+++ src/main/java/com/example/backendingsw/Service/Implementations/Impl_Asta_inversa_Service.java
@@ -1,5 +1,6 @@
 package com.example.backendingsw.Service.Implementations;
 
+import com.example.backendingsw.Model.Asta_allinglese;
 import com.example.backendingsw.Model.Asta_inversa;
 import com.example.backendingsw.Repository.Asta_inversaRepository;
 import com.example.backendingsw.Service.Interfaces.I_Asta_inversa_Service;
@@ -18,11 +19,11 @@ public class Impl_Asta_inversa_Service implements I_Asta_inversa_Service {
 
     @Override
     public List<Asta_inversa> findByCondizioneOrderByDataDiScadenzaAsc(String condizione){
-        return asta_inversaRepository.findFirst15ByCondizioneOrderByDataDiScadenzaAsc("aperta");
+        return asta_inversaRepository.findByCondizioneOrderByDataDiScadenzaAsc("aperta");
     }
     @Override
     public List<Asta_inversa> findByCondizioneOrderByIdDesc(String condizione){
-        return asta_inversaRepository.findFirst15ByCondizioneOrderByIdDesc("aperta");
+        return asta_inversaRepository.findByCondizioneOrderByIdDesc("aperta");
     }
     @Override
     public List<Asta_inversa> findByCategorieNomeAndCondizioneAperta(String nomeCategoria){
